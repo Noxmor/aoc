66375b2d50d24664725a4d702df604ba959f8987
Noxmor <lord.noxmor@gmail.com>
2026-05-27 15:02:56 +0200

    Add solution for 2015-04 part 1

---
+++ rust/2015/day04/src/part1.rs
@@ -0,0 +1,27 @@
+use md5::{Md5, Digest};
+
+pub fn solve(input: &String) {
+    let mut number = 0;
+
+    loop {
+        let mut hasher = Md5::new();
+        hasher.update(input.trim());
+        hasher.update(number.to_string());
+        let hash = hasher.finalize().iter().map(|b| format!("{:02x}", b)).collect::<String>();
+
+        let mut found_solution = true;
+        for c in hash.chars().take(5) {
+            if c != '0' {
+                found_solution = false;
+            }
+        }
+
+        if found_solution {
+            break;
+        }
+
+        number += 1;
+    }
+
+    println!("Part 1: {number}");
+}
